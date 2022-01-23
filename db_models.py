117bb11314efa2f8ddb138799538df51e1454fc8
vlad <vlad.pickalov@gmail.com>
2022-01-23 14:02:12 +0200

    added models

--- db_models.py
+++ db_models.py
@@ -26,8 +26,8 @@ class Grades(Base):
     __tablename__ = 'grades'
 
     id = Column(Integer, primary_key=True)
-    owner_id = Column(Integer, ForeignKey('users.id'))
-    owner = relationship('Users', backref='grades')
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('Users', backref='grades')
     zno_id = Column(Integer, ForeignKey('zno.id'))
     zno = relationship('Zno', backref='grades')
     grade = Column(Integer)
@@ -55,8 +55,9 @@ class Knowledge_area(Base):
     __tablename__ = 'knowledge_area'
 
     id = Column(Integer, primary_key=True)
-    code = Column(BigInteger)
+    # code = Column(BigInteger)
     name = Column(String(255))
+    university = relationship('University', backref='knowledge_area')
 
     def __repr__(self):
         return self.name
@@ -69,9 +70,26 @@ class Speciality(Base):
     name = Column(String(255))
     program = Column(String(255))
     min_rate_budget = Column(Float)
-    min_rate_pay = Column(Float)
+    average_rate_contract = Column(Float)
     area_id = Column(Integer, ForeignKey('knowledge_area.id'))
     area = relationship('Knowledge_area', backref='specialities')
+    faculty = Column(String(255))
+
+
+class Region(Base):
+    __tablename__ = 'region'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    region_coefficient = Column(Integer)
+
+
+class University(Base):
+    __tablename__ = 'university'
+
+    id = Column(Integer, primary_key=True)
+    region_id = Column(Integer, ForeignKey('region.id'))
+    region = relationship('Region', backref='university')
 
     def __repr__(self):
         return self.name
+
